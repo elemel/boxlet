96298607954be1f66f2095f9098b9cbd6cb8812e
Mikael Lind <elemel@elemel.se>
2010-03-12 21:00:44 +0100

    Removed vehicle controller. Center background image.

--- lib/boxlet/main.py
+++ lib/boxlet/main.py
@@ -377,19 +377,6 @@ class TestPlatformActor(Actor):
         body_data = self.create_body(position=position, angle=angle)
         self.create_polygon_shape(body_data, vertices=get_box_vertices(5.0, 0.1))
 
-class TestVehicleController(Controller):
-    def __init__(self, game_engine, actor):
-        super(TestVehicleController, self).__init__(game_engine)
-        self.actor = actor
-        self.left = False
-        self.right = False
-        self.torque = 5000.0
-
-    def step(self, dt):
-        if self.left or self.right:
-            torque = self.torque * (self.left - self.right)
-            self.actor.frame_data.body.ApplyTorque(torque)
-
 class TestVehicleActor(Actor):
     def __init__(self, game_engine, position=(0.0, 0.0)):
         super(TestVehicleActor, self).__init__(game_engine)
@@ -402,36 +389,27 @@ class TestVehicleActor(Actor):
                                  group_index=-self.group_index, color=(0.5, 0.5, 0.5), shading='edge')
         self.left_joint_data = self.create_revolute_joint(self.frame_data, self.left_wheel_data,
                                                           self.left_wheel_data.body.GetWorldCenter(),
-                                                          motor_speed=-20.0, max_motor_torque=10000.0)
+                                                          motor_speed=-20.0, max_motor_torque=7000.0)
         self.right_wheel_data = self.create_body(position=(position + b2Vec2(0.7, -0.4)))
         self.create_circle_shape(self.right_wheel_data, radius=0.4, density=100.0, friction=5.0,
                                  group_index=-self.group_index, color=(0.5, 0.5, 0.5), shading='edge')
         self.right_joint_data = self.create_revolute_joint(self.frame_data, self.right_wheel_data,
                                                       self.right_wheel_data.body.GetWorldCenter(),
-                                                      motor_speed=20.0, max_motor_torque=10000.0)
-        self.controller = TestVehicleController(self.game_engine, self)
-
-    def delete(self):
-        self.controller.delete()
-        super(TestVehicleActor, self).delete()
+                                                      motor_speed=20.0, max_motor_torque=7000.0)
 
     def on_key_press(self, key, modifiers):
         if key == pyglet.window.key.LEFT:
             self.right_joint_data.joint.enableMotor = True
-            self.controller.left = True
             self.right_wheel_data.body.WakeUp()
         if key == pyglet.window.key.RIGHT:
             self.left_joint_data.joint.enableMotor = True
-            self.controller.right = True
             self.left_wheel_data.body.WakeUp()
 
     def on_key_release(self, key, modifiers):
         if key == pyglet.window.key.LEFT:
             self.right_joint_data.joint.enableMotor = False
-            self.controller.left = False
         if key == pyglet.window.key.RIGHT:
             self.left_joint_data.joint.enableMotor = False
-            self.controller.right = False
 
 class CameraActor(Actor):
     def __init__(self, game_engine):
@@ -474,6 +452,8 @@ class GameEngine(object):
         self.camera_actor = CameraActor(self)
         self._init_test_actors()
         self.background_image = pyglet.resource.image('resources/images/cave.jpg')
+        self.background_image.anchor_x = self.background_image.width // 2
+        self.background_image.anchor_y = self.background_image.height // 2
         # self.lighting_image = pyglet.resource.image('cave-lighting.png')
 
     def delete(self):
@@ -518,7 +498,7 @@ class GameEngine(object):
                 controller.step(dt)
 
     def draw(self, width, height):
-        self.background_image.blit(0, 0)
+        self.background_image.blit(width // 2, height // 2)
         if self.player_actor is not None:
             self.camera_actor.position = self.player_actor.first_body_position
         with self.camera_actor.transform(width, height):
