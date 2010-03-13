cf2510ad2000d1b9dffa53800d1f71ec2bd54714
Mikael Lind <elemel@elemel.se>
2010-03-13 16:46:08 +0100

    simplified lighting class hierarchy

--- lib/boxlet/lighting.py
+++ lib/boxlet/lighting.py
@@ -35,18 +35,11 @@ class LightingManager(object):
     def free(self, light_name):
         self._free.append(light_name)
 
-class LightImpl(object):
-    def __init__(self, lighting_manager, ambient=(0.0, 0.0, 0.0, 1.0),
-                 diffuse=(0.0, 0.0, 0.0, 1.0), specular=(0.0, 0.0, 0.0, 1.0),
-                 position=(0.0, 0.0, 1.0, 0.0), enabled=True):
+class Light(object):
+    def __init__(self, lighting_manager, enabled=True):
         assert isinstance(lighting_manager, LightingManager)
         self._lighting_manager = lighting_manager
         self._light_name = self._lighting_manager.alloc()
-        self._enabled = False
-        self.ambient = ambient
-        self.diffuse = diffuse
-        self.specular = specular
-        self.position = position
         self.enabled = enabled
 
     def delete(self):
@@ -68,62 +61,33 @@ class LightImpl(object):
         else:
             glDisable(self._light_name)
 
-    @property
-    def ambient(self):
-        raise NotImplementedError()
-
-    @ambient.setter
-    def ambient(self, ambient):
-        glLightfv(self._light_name, GL_AMBIENT, (c_float * 4)(*ambient))
-
-    @property
-    def diffuse(self):
-        raise NotImplementedError()
-
-    @diffuse.setter
-    def diffuse(self, diffuse):
-        glLightfv(self._light_name, GL_DIFFUSE, (c_float * 4)(*diffuse))
-
-    @property
-    def specular(self):
-        raise NotImplementedError()
-
-    @specular.setter
-    def specular(self, specular):
-        glLightfv(self._light_name, GL_SPECULAR, (c_float * 4)(*specular))
-
-    @property
-    def position(self):
-        raise NotImplementedError()
-
-    @position.setter
-    def position(self, position):
-        glLightfv(self._light_name, GL_POSITION, (c_float * 4)(*position))
-
-class Light(object):
-    def __init__(self, impl):
-        assert isinstance(impl, LightImpl)
-        self._impl = impl
-
-    def delete(self):
-        if self._impl is not None:
-            self._impl.delete()
-            self._impl = None
-
-    @property
-    def enabled(self):
-        return self._impl.enabled
-
-    @enabled.setter
-    def enabled(self, enabled):
-        self._impl.enabled = enabled
-
 class DirectionalLight(Light):
     def __init__(self, lighting_manager, color=(1.0, 1.0, 1.0),
                  direction=(0.0, 0.0, -1.0), enabled=True):
         assert isinstance(lighting_manager, LightingManager)
-        diffuse = specular = color + (1.0,)
-        position = (-direction[0], -direction[1], -direction[2])
-        impl = LightImpl(lighting_manager, diffuse=diffuse,
-                         specular=specular, position=position, enabled=enabled)
-        super(DirectionalLight, self).__init__(impl)
+        super(DirectionalLight, self).__init__(lighting_manager, enabled)
+        self.color = color
+        self.direction = direction
+ 
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, color):
+        self._color = color
+        r, g, b = self._color
+        color = (c_float * 4)(r, g, b, 1.0)
+        glLightfv(self._light_name, GL_DIFFUSE, color)
+        glLightfv(self._light_name, GL_SPECULAR, color)
+
+    @property
+    def direction(self):
+        return self._direction
+
+    @direction.setter
+    def direction(self, direction):
+        self._direction = direction
+        x, y, z = self._direction
+        position = (c_float * 4)(-x, -y, -z, 0.0)
+        glLightfv(self._light_name, GL_POSITION, position)
